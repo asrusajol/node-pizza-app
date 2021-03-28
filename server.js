d73f881514c70e6c9234f1d27727d863c5bdb9e1
Rajib Ahamad <rajib.sub39@gmail.com>
2021-03-28 12:37:19 +0600

    First commist for app

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const { dirname } = require("path")
+const app = express()
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+
+
+const PORT = process.env.PORT || 8000
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+//set Template Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(PORT, ()=> {
+    console.log(`Listen on port ${PORT}`)
+    console.log('Server run Success !!hi!')
+})
+
