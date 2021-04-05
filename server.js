43654ab8418cc3040fda2062f5ab25692389e458
Rajib Ahamad <rajib.sub39@gmail.com>
2021-04-05 17:11:52 +0600

    Route and controler files added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const mysql = require('mysql');
 const express = require("express")
 const { dirname } = require("path")
 const app = express()
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
+
+
+//Database Connection
+
 
 //Asseet
 app.use(express.static('public'))
@@ -13,23 +18,10 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
 
-//Routes
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+//Routes call
+require('./routes/web.js')(app)
 
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
-
+//server run
 const PORT = process.env.PORT || 8000
 app.listen(PORT, ()=> {
     console.log(`Listen on port ${PORT}`)
