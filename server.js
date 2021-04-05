4c2526c4fad36b6bbb61ccd57a4033ba00d08659
Rajib Ahamad <rajib.sub39@gmail.com>
2021-04-05 11:58:32 +0600

    Auth login/register pases added

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ app.get('/cart',(req,res)=>{
     res.render('customers/cart')
 })
 
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, ()=> {
