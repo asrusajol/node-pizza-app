1dd89babee91ff47b72b6a8e8d84b849a7385909
Rajib Home <asru.sajol@gmail.com>
2021-04-07 22:56:19 +0600

    Register logig addred and stored to DB

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require('../../models/user.js')
+const bcrypt = require('bcrypt')
+
 function authController(){
     return{
         login(req,res){
@@ -7,8 +10,40 @@ function authController(){
     register(req,res){
             res.render('auth/register')
         },
-    postRegister(req,res){
+    async postRegister(req,res){
         const { name, email, password } = req.body
+        //User Validatation
+        if(!name || !email || ! password){
+            req.flash('error','All fields are required !!!')
+            req.flash('name',name)
+            req.flash('email',email)
+            return res.redirect('/register')
+        }
+        //email validatation
+        User.exists({email: email},(err, result) =>{
+            if(result){
+                req.flash('error','Email already exist !!!')
+                req.flash('name',name)
+                req.flash('email',email)
+                return res.redirect('/register')
+            }
+        })
+
+        //password hashing
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        //user creation to database
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        })
+        user.save().then((user)=>{
+            return res.redirect('/')
+        }).catch(err =>{
+            req.flash('error','Something went wrong')
+                return res.redirect('/register')
+        })
         console.log(req.body)
     }
     }
